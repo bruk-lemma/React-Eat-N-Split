8b5924ff656a7402f95e71e6d4b98a216ad5ed72
bruk-lemma <bruklemma2017@gmail.com>
2023-06-26 11:48:47 +0300

    created contolled elemnts in the split bill component

--- src/App.js
+++ src/App.js
@@ -145,17 +145,41 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : 0;
+  const [whoIsPaying, setWhoIsPaying] = useState("");
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name} </h2>
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
       <label>🧍‍♀️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
       <label>👫 {selectedFriend.name} expense</label>
-      <input type="text" {...(selectedFriend ? `disabled` : "")} />
+      <input
+        type="text"
+        {...(selectedFriend ? `disabled` : "")}
+        value={paidByFriend}
+      />
       <label>🤑 Who is paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">user</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
